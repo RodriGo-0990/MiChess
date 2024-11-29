5ce2fc996eb6d1839b3c920f65e7e8b685aa77b9
rodrigo.rodrigues <rodrigorodrigues_0990@hotmail.com>
2024-11-28 22:28:33 -0300

    responsividade e menu mobile

--- javaScript/navegacao.js
+++ javaScript/navegacao.js
@@ -10,11 +10,23 @@ document.addEventListener("DOMContentLoaded", () => {
     let campos = [];
     campos.push(wrapper, wrapperAbout, wrapperSkills, wrapperXp, wrapperProjects, wrapperJogo);
     //menu
+    let home = document.getElementById('homemenu');
     let about = document.getElementById('aboutmenu');
     let skills = document.getElementById('skillsmenu');
     let xp = document.getElementById('xpmenu');
     let projects = document.getElementById('projmenu');
     let play = document.getElementById('playmenu');
+
+    //menu-mobile
+    let hamburguer_icon = document.querySelector(".hamburguer-menu");
+    let close_icon = document.querySelector(".close-menu");
+    let menu_mobile = document.querySelector(".menu-mobile");
+
+    let about_mobile = document.getElementById('aboutmenu-mobile');
+    let skills_mobile = document.getElementById('skillsmenu-mobile');
+    let xp_mobile = document.getElementById('xpmenu-mobile');
+    let projects_mobile = document.getElementById('projmenu-mobile');
+    let play_mobile = document.getElementById('playmenu-mobile');
 
     //função para navegação
     function movePage(num) {
@@ -25,7 +37,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     //listeners
-    wrapper.addEventListener("click", function () {
+    home.addEventListener("click", function () {
         movePage(0);
     });
     about.addEventListener("click", function () {
@@ -43,6 +55,36 @@ document.addEventListener("DOMContentLoaded", () => {
     play.addEventListener("click", function () {
         movePage(5);
     });
+
+    //listeners to mobile
+    about_mobile.addEventListener("click", function () {
+        movePage(1);
+        menu_mobile.style.display = "none";
+    });
+    skills_mobile.addEventListener("click", function () {
+        movePage(2);
+        menu_mobile.style.display = "none";
+    });
+    xp_mobile.addEventListener("click", function () {
+        movePage(3);
+        menu_mobile.style.display = "none";
+    });
+    projects_mobile.addEventListener("click", function () {
+        movePage(4);
+        menu_mobile.style.display = "none";
+    });
+    play_mobile.addEventListener("click", function () {
+        movePage(5);
+        menu_mobile.style.display = "none";
+    });
+
+    hamburguer_icon.addEventListener("click", function () {
+        menu_mobile.style.display = "flex";
+    })
+    close_icon.addEventListener("click", function () {
+        menu_mobile.style.display = "none";
+    })
+
 })
 
 //SCROLLREVEAL
